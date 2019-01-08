d00b96ee71736b7cea5c6b577ff18351916b81d1
Jeremy Atkinson <jchatkinson@gmail.com>
2019-01-07 17:14:54 -0800

    Add project files.

---
+++ ETABS_Plugin/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ETABSv17;
+
+namespace ETABS_Plugin
+{
+    public partial class Form1 : Form
+    {
+        private cPluginCallback _Plugin = null;
+        private cSapModel _SapModel = null;
+
+        public Form1(ref cSapModel SapModel, ref cPluginCallback Plugin)
+        {
+            _Plugin = Plugin;
+            _SapModel = SapModel;
+
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // do setup things here
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            // must include a call to finish()
+            _Plugin.Finish(0);
+        }
+    }
+}
---
+++ ETABS_Plugin/cPlugin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETABS_Plugin
+{
+    public class cPlugin
+    {
+        public void Main(ref ETABSv17.cSapModel SapModel, ref ETABSv17.cPluginCallback ISapPlugin)
+        {
+            Form1 form = new Form1(ref SapModel, ref ISapPlugin);
+            form.Show();
+        }
+
+        public long Info(ref string Text)
+        {
+            Text = "my first plugin\n";
+            return 0;
+        }
+    }
+}
